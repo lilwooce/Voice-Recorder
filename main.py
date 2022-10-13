f022ba6a9b32f0fe4b4df3cd19eb2bc888ead890
lilwooce <kryptstep@gmail.com>
2022-10-12 21:24:37 -0400

    Updates

--- main.py
+++ main.py
@@ -1,77 +1,55 @@
 import discord
+import os
+import sys
+import traceback
+import requests
 from discord.ext import commands
+from dotenv import load_dotenv
 from discord.ext.audiorec import NativeVoiceClient  # important!
 import random
 from time import sleep
 
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+updatePURL = os.getenv('UP_URL')
+removePURL = os.getenv('RP_URL')
+getPURL = os.getenv('GP_URL')
+
 intents = discord.Intents().all()
-client = commands.Bot(command_prefix="$", intents=intents)
-client.remove_command('help')
-token='MTAyNzY1MzAxOTk4MDAxNzcwNA.G2uadU.UFFAkFg4V4AaHm-KYZw5e0fHeUQcY_OwEQxCY4'
+intents.members = True
+bot = commands.Bot(command_prefix="$", intents=intents)
+bot.remove_command('help')
 
-userTimer = 5
+def get_prefix(client, message):
+    obj = {"f1": "server", "q1": message.guild.id}
+    result = requests.get(getPURL, params=obj, headers={"User-Agent": "XY"})
+    prefix = result.text.strip('\"')
+    return prefix
 
-@client.event
-async def on_ready():
-    print('im ready')
+initial_extensions = {
+    "cogs.Config",
+    "cogs.Snipe",
+    "cogs.Music"
+}
 
+@bot.event
+async def on_guild_join(guild):
+    obj = {"f1": guild.id, "q1": '!'}
+    result = requests.post(updatePURL, data=obj, headers={"User-Agent": "XY"})
+    print(result.status_code)
 
-@client.command()
-async def help(ctx):
-    embedVar = discord.Embed(title="here are my commands!",
-                             description="nuser **$join** to start the recording\nuser **$stop** to stop the recording", color=0x546e7a)
-    await ctx.send(embed=embedVar)
-
-
-@client.command()
-async def join(ctx: commands.Context):
-    channel: discord.VoiceChannel = ctx.author.voice.channel
-    if ctx.voice_client is not None:
-        return await ctx.voice_client.move_to(channel)
-    await channel.connect(cls=NativeVoiceClient)
-    await ctx.invoke(client.get_command('rec'))
-
-
-@client.command()
-async def test(ctx):
-    await ctx.send('hello im alive!')
+@bot.event
+async def on_guild_remove(guild):
+    obj = {"q1": guild.id}
+    result = requests.post(removePURL, data=obj, headers={"User-Agent": "XY"})
+    print(result.status_code)
 
 
-@client.command()
-async def rec(ctx, name=None):
-    ctx.voice_client.record(lambda e: print(f"Error Recording Message. Exceptioxn: {e}"))
-    embedVar = discord.Embed(title="Started the Recording!",
-                             description="use $stop to stop!", color=0x546e7a)
-    message = await ctx.send(embed=embedVar)
+for extension in initial_extensions:
+    try:
+        bot.load_extension(extension)
+    except Exception as e:
+        print(f'Failed to load extension {extension}.', file=sys.stderr)
+        traceback.print_exc()
 
-    sleep(userTimer)
-
-    if not ctx.voice_client.is_recording():
-        return
-    sMessage = await ctx.send(f'Stopping the Recording')
-
-    wav_bytes = await ctx.voice_client.stop_record()
-
-    if not name:
-        name = str(random.randint(000000, 999999))
-    with open(f'{name}.wav', 'wb') as f:
-        f.write(wav_bytes)
-    await ctx.channel.send(file=discord.File(f.name))
-    await message.delete()
-    await sMessage.delete()
-    await ctx.message.delete()
-    await ctx.voice_client.disconnect()
-
-@rec.before_invoke
-async def ensure_voice(ctx):
-    if ctx.voice_client is None:
-        if ctx.author.voice:
-            await ctx.author.voice.channel.connect(cls=NativeVoiceClient)
-        else:
-            await ctx.send("You are not connected to a voice channel.")
-            raise commands.CommandError(
-                "Author not connected to a voice channel.")
-    elif ctx.voice_client.is_playing():
-        ctx.voice_client.stop()
-
-client.run(token)
+bot.run(token)
