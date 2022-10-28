d1b078dedd48205169a094444559a298bf525eda
lilwooce <kryptstep@gmail.com>
2022-10-27 23:05:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ async def on_guild_join(guild):
             print("this is a bot")
             continue
         print("this is not a bot")
-        premStatus = requests.get(getPremium, params={"f1": "isPremium", "f2": member.id}, headers=header)
+        premStatus = requests.get(getPremium, params={"f1": "isPremium", "f2": member.id, "f3": "userID"}, headers=header)
         premStatus = premStatus.text.replace('"', '')
         if (not exists(member.id)):
             addUser(member.id)
